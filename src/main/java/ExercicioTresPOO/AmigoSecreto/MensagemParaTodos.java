08b5e05b0ddc39e21330f738bea48f48d399ed22
gabriela.oliveira <gabisoliporto@gmail.com>
2024-07-18 10:31:05 -0300

    Att

---
+++ src/main/java/ExercicioTresPOO/AmigoSecreto/MensagemParaTodos.java
@@ -0,0 +1,11 @@
+package ExercicioTresPOO.AmigoSecreto;
+
+public class MensagemParaTodos extends Mensagem{
+    public MensagemParaTodos(String texto, String emailRemetente, boolean anonima){
+        super(texto, emailRemetente, anonima);
+    }
+    public String getTextoCompletoAExibir(){
+        return null;
+    }
+
+}
