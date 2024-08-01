f24442a97b1c17974e0377a67c24a0a6cdd751f9
gabriela.oliveira <gabisoliporto@gmail.com>
2024-08-01 10:38:10 -0300

    att

--- src/main/java/AmigoSecreto/AmigoSecreto/SistemaAmigoMap.java
+++ src/main/java/AmigoSecreto/AmigoSecreto/SistemaAmigoMap.java
@@ -37,4 +37,51 @@ public class SistemaAmigoMap {
         throw new AmigoNaoCadastradoException("Amigo não cadastrado");
     }
 
+    /**
+     * public void enviarMensagemParaTodosMap(String texto, String emailRemetente, boolean ehAnonima){
+     *         this.mensagens.put(mensagens.toString(), new MensagemParaTodos(texto, emailRemetente, ehAnonima));
+     *     }
+     *     public void enviarMensagemParaAlguemMap(String texto, String emailRemetente, String emailDestinatario, boolean ehAnonima){
+     *         this.mensagens.put(mensagens.toString(), new MensagemParaAlguem(texto, emailRemetente, emailDestinatario, ehAnonima));
+     *     }
+     *     public Map<String, Mensagem> pesquisaMensagensAnonimasMap()throws ListaVaziaException{
+     *         Map<String,Mensagem> mensagensAnonimas = new HashMap<>();
+     *         if (mensagens.isEmpty())throw new ListaVaziaException("A lista de mensagens está vazia");
+     *         for (Mensagem mensagemAnonimas: this.mensagens.values()){
+     *             if (mensagemAnonimas.isAnonima()){
+     *                 mensagensAnonimas.put(mensagemAnonimas.getTextoCompletoAExibir() , mensagemAnonimas);
+     *             }
+     *         }
+     *         return mensagensAnonimas;
+     *     }
+     *
+     *     public Map<String, Mensagem> pesquisaTodasAsMensagensMap() throws ListaVaziaException{
+     *         if (this.mensagens.isEmpty()) throw new ListaVaziaException("Lista de amigos está vazia");
+     *         return this.mensagens;
+     *     }
+     *
+     *     public void configuraAmigoSecretoDeMap(String emailDaPessoa, String emailAmigoSorteado) throws ListaVaziaException, AmigoNaoCadastradoException{
+     *         if (this.amigos.isEmpty())throw new ListaVaziaException("Lista de amigos está vazia");
+     *         for (Amigo amigoPesq: this.amigos.values()){
+     *             if (amigoPesq.getEmail().equals(emailDaPessoa)){
+     *                 amigoPesq.setEmailAmigoSorteado(emailAmigoSorteado);
+     *             }
+     *         }
+     *     }
+     *     public String pesquisaAmigoSecretoDeMap(String emailDaPessoa) throws ListaVaziaException, AmigoNaoCadastradoException, AmigoSorteadoNaoCadastradoException {
+     *         if (this.amigos.isEmpty()) throw new ListaVaziaException("Lista de amigos está vazia");
+     *
+     *         for (Amigo amigoSecreto: this.amigos.values()){
+     *             if (amigoSecreto.getEmail().equals(emailDaPessoa)){
+     *                 if (amigoSecreto.getEmailAmigoSorteado().equals("")){
+     *                     throw new AmigoSorteadoNaoCadastradoException("Não houve um sorteio para esse amigo");
+     *                 }else{
+     *                     return amigoSecreto.getEmailAmigoSorteado();
+     *                 }
+     *             }
+     *         }
+     *         throw new AmigoNaoCadastradoException("Amigo desse email não cadastrado");
+     *     }
+     */
+
 }
